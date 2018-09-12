519bbf2e6f380ed8304dd8933293409639e8a354
Tellycoin <43203950+Tellycoin@users.noreply.github.com>
2018-09-12 12:23:00 +0200

    Initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Tellycoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "TellycoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
